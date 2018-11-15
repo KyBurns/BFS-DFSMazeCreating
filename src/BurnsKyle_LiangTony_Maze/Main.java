a5d3ae6cb4f580cbde990822cdd71c5e10c2285e
Tony1120 <jliang1120tony@gmail.com>
2018-11-14 20:49:44 -0800

    finish implementing the creating maze part

--- src/BurnsKyle_LiangTony_Maze/Main.java
+++ src/BurnsKyle_LiangTony_Maze/Main.java
@@ -4,9 +4,10 @@ public class Main
 {
    public static void main(String[] args)
    {
-      Maze m = new Maze(4, 0);
+      Maze m = new Maze(7, 0);
       m.generateRoom();
-      m.printMaze();
+
       m.removeWalls();
+      m.printMaze();
    }
 }
--- src/BurnsKyle_LiangTony_Maze/Maze.java
+++ src/BurnsKyle_LiangTony_Maze/Maze.java
@@ -7,6 +7,8 @@ import java.util.Stack;
 public class Maze {
 
    private char[][] maze;
+   private boolean[][] tfmaze;
+
    private int size;
    private int arrayLength;
    private Random myRandGen;
@@ -17,8 +19,19 @@ public class Maze {
       myRandGen=new java.util.Random(seed);
       arrayLength = 2 * size + 1;
       maze = new char[arrayLength][arrayLength];
+      tfmaze = new boolean[arrayLength][arrayLength];
+      for(int y = 1; y < arrayLength; y += 2) {
+         for (int x = 1; x < arrayLength; x +=2) {
+            tfmaze[y][x] = false;
+         }
+      }
+
+
+
+
 
 	}
+
 	public void generateRoom() {
 		//System.out.println(maze[0].length);
 	      for (int i = 0; i < arrayLength; i += 2) {
@@ -39,55 +52,44 @@ public class Maze {
 	            }
 	         }
 	      }
-
-
 	      maze[0][1] = ' ';
 	      maze[arrayLength - 1][arrayLength - 2] = ' ';
 	}
+//=======================================================================================================================================
 	public void removeWalls() {
-		Stack<int[]> CellStack = new Stack<int[]>();
+		Stack<Cell> cellStack = new Stack<>();
 		int totalCells = size*size;
-		int[] currentCell = new int[2];
-		currentCell[0] = 1;
-		currentCell[1] = 1;
+		Cell currentCell = new Cell(1,1);
+
 		int visitedCells = 1;
-		//while(visitedCells<totalCells) {
-			ArrayList<int[]> neighbors = new ArrayList<int[]>();
-			if(currentCell[1]-2>0 && maze[currentCell[1]][currentCell[0]-1] =='|') {
-				System.out.print("left");
-				int[] leftNeighbor = new int[2];
-				leftNeighbor[0] = currentCell[0]-2;
-				leftNeighbor[1] = currentCell[1];
-				neighbors.add(leftNeighbor);
-			}
-			System.out.println(currentCell[0]+2);
-			System.out.println(maze[currentCell[0]+1][currentCell[1]]);
-			if(currentCell[1]+2<arrayLength && maze[currentCell[1]][currentCell[0]+1] =='|') {
-				System.out.print("right");
-				int[] rightNeighbor = new int[2];
-				rightNeighbor[0] = currentCell[0]+2;
-				rightNeighbor[1] = currentCell[1];
-				neighbors.add(rightNeighbor);
-			}
-			if(currentCell[0]-2>0 && maze[currentCell[1]-1][currentCell[0]] =='-') {
-				System.out.print("up");
-				int[] upNeighbor = new int[2];
-				upNeighbor[0] = currentCell[0];
-				upNeighbor[1] = currentCell[1]-2;
-				neighbors.add(upNeighbor);
-			}
-			if(currentCell[0]+2>0 && maze[currentCell[1]+1][currentCell[0]] =='-') {
-				System.out.print("down");
-				int[] downNeighbor = new int[2];
-				downNeighbor[0] = currentCell[0];
-				downNeighbor[1] = currentCell[1]+2;
-				neighbors.add(downNeighbor);
-			}
-			System.out.println(neighbors);
-		//}
+
+      while(visitedCells<totalCells) {
+
+         //System.out.println("current cell is " + currentCell);
+         ArrayList<Cell> neighbors = findNeighbors(currentCell);
+
+         //printCellStack(cellStack);
+         if (neighbors.size() > 0) {
+            Cell randomNeighbor = neighbors.get((int) (myrandom() * neighbors.size()));
+            //System.out.println("ranN is " + randomNeighbor);
+            tfmaze[randomNeighbor.getY()][randomNeighbor.getX()] = true;
+            int wallx = (randomNeighbor.getX() + currentCell.getX()) / 2;
+            int wally = (randomNeighbor.getY() + currentCell.getY()) / 2;
+
+            maze[wally][wallx] = ' ';
+
+            cellStack.push(currentCell);
+            currentCell = randomNeighbor;
+            visitedCells += 1;
+         } else {
+            currentCell = cellStack.pop();
+         }
+
+      }
 	}
 
-	public void printMaze() {
+   //=======================================================================================================================================
+   public void printMaze() {
       for (int i = 0; i < this.maze.length; i++) {
          for (int j = 0; j < this.maze.length; j++) {
             System.out.print(this.maze[i][j]);
@@ -95,10 +97,57 @@ public class Maze {
          System.out.println();
       }
    }
-	
-	double myrandom() {
+   //=======================================================================================================================================
+   //=======================================================================================================================================
+   //=======================================================================================================================================
+   //=======================================================================================================================================
+	private double myrandom() {
 		return myRandGen.nextDouble(); //random in 0-1
 	}
 
+   //=======================================================================================================================================
+//	private void printNeighbors (ArrayList<int[]> neighbors) {
+//      for (int[] cell: neighbors) {
+//         System.out.print(Arrays.toString(cell));
+//      }
+//   }
+   //=======================================================================================================================================
+   private ArrayList<Cell> findNeighbors (Cell currentCell) {
+      ArrayList<Cell> neighbors = new ArrayList<>();
+      int curx = currentCell.getX();
+      int cury = currentCell.getY();
+
+
+      if(  curx - 2 > 0  && !tfmaze[cury][curx-2] ) {
+         //System.out.println("left");
+         Cell leftNeighbor = new Cell(curx-2, cury);
+         neighbors.add(leftNeighbor);
+
+      }
+
+
+      if( curx + 2 < arrayLength  && !tfmaze[cury][curx+2]) {
+         //System.out.println("right");
+         Cell rightNeighbor = new Cell(curx+2, cury);
+         neighbors.add(rightNeighbor);
+      }
+      //&& maze[cury - 1][curx] =='-'
+      if(cury-2 > 0  && !tfmaze[cury - 2][curx] ) {
+         //System.out.println("up");
+         Cell upNeighbor = new Cell(curx, cury - 2);
+         neighbors.add(upNeighbor);
+      }
+
+      if( cury + 2 < arrayLength && !tfmaze[cury + 2][curx]  ) {
+         //System.out.println("down");
+
+         Cell downNeighbor = new Cell(curx, cury + 2);
+         neighbors.add(downNeighbor);
+
+      }
+      //printNeighbors(neighbors);
+
+      return neighbors;
+   }
 
 }
